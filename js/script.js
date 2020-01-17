612665a517ba99cd776ee24f28576205eb1e1415
Piotr <piotr.wach122@gmail.com>
2020-01-17 02:22:47 +0100

    Add if/else function

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ let computerMove = 'nieznany ruch';
 if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
-    computerMove = 'papier'
+    computerMove = 'papier';
 } else if (randomNumber == 3) {
-    computerMove = 'nożyce'
+    computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
@@ -23,9 +23,31 @@ let playerMove = 'nieznany ruch';
 if (playerInput == '1') {
     playerMove = 'kamień';
 } else if {
-    playerMove = 'papier'
+    playerMove = 'papier';
 } else if {
-    playerMove = 'nożyce'
+    playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (playerInput > 3) {
+    printMessage('nieznany ruch');
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrałeś')
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Ja wygrałem');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Mamy remis');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Ja wygrałem');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrałeś');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Mamy remis');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Ja wygrałem');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrałeś')
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Mamy remis');
+}
